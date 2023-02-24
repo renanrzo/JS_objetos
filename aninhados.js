3fa3d2e488bbd5a8debfb221a011126333e35221
renanrzo <arenan1988@gmail.com>
2023-02-24 07:08:52 -0300

    update 24-02-2023

---
+++ aninhados.js
@@ -0,0 +1,15 @@
+const cliente = {
+    nome: "Joao",
+    idade: 24,
+    email: "joao@firma.com",
+    telefone: ["1155558550", "1144444444"],
+};
+
+cliente.endereco = {
+    rua: "R Joseph Climber",
+    numero: 1337,
+    apartamento: true,
+    complemento: "ap 934",
+};
+
+console.log(cliente.endereco)
---
+++ tipos.js
@@ -0,0 +1,8 @@
+const cliente = {
+    nome: "Joao",
+    idade: 24,
+    email: "joao@firma.com",
+    telefone: ["1155558550", "1144444444"],
+};
+
+console.log(cliente.telefone);
